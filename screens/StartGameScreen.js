b0797c62bf96a6c732e98bb233ee0c6d0aae264a
Duy Nguyen <duy_nobita@icloud.com>
2020-03-13 01:36:17 -0400

    Created NumberContainer component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet, Button, TouchableWithoutFeedback, Keyboard, Ale
 
 import Card from '../components/Card'
 import Input from '../components/Input'
+import NumberContainer from '../components/NumberContainer'
 import Colors from '../constants/colors'
 
 const StartGameScreen = props => {
@@ -31,12 +32,21 @@ const StartGameScreen = props => {
         }
         setConfirmed(true)
         setSelectedNumber(parseInt(chosenNumber))
-        setEnteredValue()
+        setEnteredValue('')
+        Keyboard.dismiss()
     }
     let confirmedOutput
 
     if (confirmed) {
-        confirmedOutput = <Text>Chosen Number: {selectedNumber}</Text>
+        confirmedOutput = (
+            <Card style={styles.summaryContainer}>
+                <Text>You Selected</Text>
+                <View>
+                    <NumberContainer>{selectedNumber}</NumberContainer>
+                    <Button title="START GAME"/>
+                </View>
+            </Card>
+        )
     }
 
     return (
@@ -102,6 +112,10 @@ const styles = StyleSheet.create({
     input: {
         width: 50,
         textAlign: 'center'
+    },
+    summaryContainer: {
+        marginTop: 20,
+        alignItems: 'center'
     }
 })
 
